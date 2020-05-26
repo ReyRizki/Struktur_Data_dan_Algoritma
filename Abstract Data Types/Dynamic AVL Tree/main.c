1afcceac97bb323049ed1c8c45d19385f8705639
ReyRizki <reynaldirizki97.rr@gmail.com>
2020-05-26 09:20:14 +0700

    bst

---
+++ Abstract Data Types/Dynamic AVL Tree/main.c
@@ -0,0 +1,43 @@
+#include "tree.h"
+
+#ifndef MACROS
+#define MACROS
+
+#define u unsigned
+#define s short
+#define ll long long
+#define REP(i, a, b) for (i = (a); i < (b); i++)
+#define REPR(i, a, b) for (i = (a-1); i >= (b); i--)
+
+#endif
+
+int main()
+{
+    TreeRoot root = createTree();
+
+    insertNode(&root, 50);
+    insertNode(&root, 30);
+    insertNode(&root, 70);
+    insertNode(&root, 20);
+    insertNode(&root, 40);
+    insertNode(&root, 60);
+    insertNode(&root, 80);
+
+    preorder(root);
+    printf("\n");
+
+    deleteNode(&root, 20);
+    preorder(root);
+    printf("\n");
+
+    deleteNode(&root, 30);
+    preorder(root);
+    printf("\n");
+
+    deleteNode(&root, 50);
+    preorder(root);
+    printf("\n");
+
+    deleteTree(&root);
+    printf(isTreeEmpty(root) ? "Yes\n" : "No\n");
+}
